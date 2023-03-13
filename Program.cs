d1c4537501360840ac1767b1c993b611d2857f7f
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2023-03-13 18:07:06 -0300

    ta quase

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+//sistema de divisão quase funcionando
+int divisao1, divisao2, divisaoResultado;
+
+Console.Write("Você vai dividir?:");
+divisao1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Com quantos?:");
+divisao2 = Convert.ToInt32(Console.ReadLine());
+
+divisaoResultado =  divisao1 / divisao2;
+
+Console.Write($"Resultado:{divisaoResultado}\n");
+
+if(divisao1 == 0)
+{
+    Console.WriteLine("com 0 não dá né gênio!");
+}
+if(divisao2 == 0)
+{
+    Console.WriteLine("com 0 não dá né gênio!");
+}
